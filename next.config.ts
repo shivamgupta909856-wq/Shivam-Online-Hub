4f4a8c9df1315153f83d4039c72caf1dfbffa029
shivamgupta909856-wq <shivamgupta909856@gmail.com>
2026-08-19 11:52:32 +0530

    Fix Github Pagesasset paths

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/shivam-online-hub",
+  basePath: "/Shivam-Online-Hub",
   images: {
     unoptimized: true,
   },
